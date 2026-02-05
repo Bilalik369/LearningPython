45e39c0544fb95840ddc25a73cd04216f1919092
bilal iken <bilal11iken@gmail.com>
2026-02-05 20:09:36 +0100

    add

--- ml.py
+++ ml.py
@@ -8,7 +8,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 
-
 data = {
     "study_hours": [2, 4, 6, 8, 10, 3, 5, 7, 9, 1],
     "absences":   [5, 3, 2, 1, 0, 6, 3, 2, 1, 7],
@@ -17,35 +16,22 @@ data = {
 }
 
 df = pd.DataFrame(data)
-
 print("Dataset :")
 print(df)
 
 
-
-
-X = df[["study_hours", "absences", "participation"]]  
-y = df["final_grade"]                                
-
+X = df[["study_hours", "absences", "participation"]]
+y = df["final_grade"]
 
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-
 model = LinearRegression()
-
 model.fit(X_train, y_train)
 
-
-
-
-
 y_pred = model.predict(X_test)
-
-
-
 
 mse = mean_squared_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
@@ -53,20 +39,14 @@ r2 = r2_score(y_test, y_pred)
 print("\nRésultats :")
 print("MSE :", mse)
 print("R² :", r2)
-
-
-
-
-new_student = np.array([[6, 2, 70]])
+new_student = pd.DataFrame([[6, 2, 70]], columns=["study_hours", "absences", "participation"])
 
 predicted_grade = model.predict(new_student)
-
 print("\nNote prédite :", round(predicted_grade[0], 2))
 
 
-
-
-plt.scatter(y_test, y_pred)
+plt.scatter(y_test, y_pred, color="blue")
+plt.plot([y.min(), y.max()], [y.min(), y.max()], color="red", linestyle="--") 
 plt.xlabel("Vraie Note")
 plt.ylabel("Note Prédite")
 plt.title("Prédiction vs Réalité")
