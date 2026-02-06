db8d48aee882fc282045ff47960f65506687b447
bilal iken <bilal11iken@gmail.com>
2026-02-06 16:21:06 +0100

    tp

---
+++ TP/tp1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+x = 2 * np.random.random((100, 1))   
+y = 4 + 3 * x + np.random.randn(100, 1)
+
+
+plt.scatter(x, y, color="blue")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Synthetic Data")
+plt.show()
