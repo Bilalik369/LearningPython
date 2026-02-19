1a663024ef7e497d7cad77e7671cb3f636f25b6b
bilal iken <bilal11iken@gmail.com>
2026-02-19 13:14:04 +0000

    add

--- td.py
+++ td.py
@@ -1,20 +1,34 @@
-class Etudaint :
+class Etudaint:
     def __init__(self , id , nom , prenom , note):
-        self.id =id
+        self.id = id
         self.nom = nom
         self.prenom = prenom
-        self.note =note
+        self.__note = note  
 
-
-    def afficher(self) :
-        print("les information de l etudaitnt" ,self.id , self.nom , self.prenom , self.note)
+    def afficher(self):
+        print("Les informations de l'etudiant :", 
+              self.id, self.nom, self.prenom, self.__note)
     
-    def est_admis(sefl ):
-        if sefl.note >= 10 :
-            print("admis")
-        else :
-            print("non admis")
+    def est_admis(self):
+        if self.__note >= 10:
+            print("Admis")
+        else:
+            print("Non admis")
 
-e1 = Etudaint(2, "ahmed" , "brahim" , 12)
+    def getNote(self):
+        return self.__note
+    
+    def setNote(self , val):
+        if 0 <= val <= 20:
+            self.__note = val
+        else:
+            print("Note invalide")
+
+
+e1 = Etudaint(2, "ahmed", "brahim", 12)
+
 e1.afficher()
 e1.est_admis()
+
+e1.setNote(18)
+print("Nouvelle note :", e1.getNote())
